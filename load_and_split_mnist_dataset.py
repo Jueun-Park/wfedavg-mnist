d7e4829525953d3df1674b1de863b9505e6a0bc7
Jueun-Park <elpion19@gmail.com>
2020-03-28 01:26:39 +0900

    add function: load as tensor

--- load_and_split_mnist_dataset.py
+++ load_and_split_mnist_dataset.py
@@ -6,11 +6,16 @@ from torch.utils.data import TensorDataset, DataLoader, Subset
 from learner import Learner
 
 
-def load_and_split_mnist_dataset():
+def load_mnist():
     train_set = datasets.MNIST('./data', train=True, download=True, transform=transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]))
     test_set = datasets.MNIST('./data', train=False, download=True,  transform=transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]))
+    return train_set, test_set
+
+
+def load_and_split_mnist_dataset():
+    train_set, test_set = load_mnist()
 
     x_train = train_set.data.numpy()
     y_train = train_set.targets.numpy()
@@ -21,30 +26,33 @@ def load_and_split_mnist_dataset():
     train_dataset = {}
     for i in range(10):
         train_dataset[i] = Subset(train_set, *np.where(y_train == i))
-        print(np.where(y_train == i)[0].shape)
 
     valid_dataset = {}
     for i in range(10):
         valid_dataset[i] = Subset(test_set, *np.where(y_valid == i))
-        print(np.where(y_valid == i)[0].shape)
 
     return train_dataset, valid_dataset
 
 
-if __name__ == "__main__":
-    use_cuda = False
-    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+def load_and_split_mnist_tensor():
+    train_set, test_set = load_mnist()
 
-    td, vd = load_and_split_mnist_dataset()
-    train_dataloaders = {}
-    valid_dataloaders = {}
-    for i in range(10):
-        train_dataloaders[i] = DataLoader(
-            td[i], batch_size=64, shuffle=True, **kwargs)
-        valid_dataloaders[i] = DataLoader(
-            vd[i], batch_size=64, shuffle=True, **kwargs)
+    x_train = train_set.data.numpy()
+    y_train = train_set.targets.numpy()
 
+    x_valid = test_set.data.numpy()
+    y_valid = test_set.targets.numpy()
+
+    train_x_tensors = {}
+    train_y_tensors = {}
     for i in range(10):
-        learner = Learner(train_dataloaders[i], valid_dataloaders[i])
-        learner.learn(10)
-        learner.save(f"./model/subenv_{i}")
+        train_x_tensors[i] = torch.Tensor(x_train[y_train == i])
+        train_y_tensors[i] = np.full(shape=(x_train.shape[0]), fill_value=i)
+
+    valid_x_tensors = {}
+    valid_y_tensors = {}
+    for i in range(10):
+        valid_x_tensors[i] = torch.Tensor(x_valid[y_valid == i])
+        valid_y_tensors[i] = np.full(shape=(x_valid.shape[0]), fill_value=i)
+
+    return train_x_tensors, train_y_tensors, valid_x_tensors, valid_y_tensors
