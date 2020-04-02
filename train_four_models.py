d65dd9fe43a9e41dba6335d6e3d51fa321db912d
Jueun-Park <elpion19@gmail.com>
2020-04-02 14:28:57 +0900

    refactor

--- train_four_models.py
+++ train_four_models.py
@@ -9,7 +9,7 @@ for i in range(0, 8, 2):
     td, vd = concat_data(list(range(10)[i:i+4]))
     tdl = DataLoader(td, batch_size=64, shuffle=True, num_workers=NUM_WORKERS)
     vdl = DataLoader(vd, batch_size=64, shuffle=True, num_workers=NUM_WORKERS)
-    learner = Learner(tdl, vdl, lr=0.005, log_interval=100, tensorboard=True, tensorboard_comment=f"{i}-{i+4}")
+    learner = Learner(tdl, vdl, lr=0.005, log_interval=100,
+                      tensorboard=True, tensorboard_comment=f"{i}-{i+4}")
     learner.learn(5)
     learner.save(f"model/subenv_{i}-{i+4}")
-    
