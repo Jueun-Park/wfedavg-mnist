b47dba3bd60a2908cac086af59d875e3ded83f26
Jueun-Park <elpion19@gmail.com>
2020-04-02 14:29:10 +0900

    train more epocs

--- train_four_rnds.py
+++ train_four_rnds.py
@@ -2,11 +2,13 @@ from rnd import RandomNetworkDistillation
 from load_and_split_mnist_dataset import concat_data
 
 NUM_WORKERS = 4
+epochs = 100
+
 
 for i in range(0, 8, 2):
     td, vd = concat_data(list(range(10))[i:i+4], mode="tensor")
     rnd = RandomNetworkDistillation(
         log_interval=100, lr=1e-5, use_cuda=False, verbose=1, log_tensorboard=True, path=f"model/subenv_{i}-{i+4}/rnd_model/")
     rnd.set_data(td, vd)
-    rnd.learn(10)
+    rnd.learn(epochs)
     rnd.save()
