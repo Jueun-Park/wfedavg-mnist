cf5b81ec6a1c3e396c79281b51fd277d03c63fc2
jueun-park <elpion19@gmail.com>
2020-04-14 13:41:16 +0900

    save client models

--- script_save_clients_wfed.py
+++ script_save_clients_wfed.py
@@ -1,6 +1,7 @@
 from os import system
+from info import num_models
 
 program = "wfedavg_save_clients.py"
-num_clients = 4
-for i in range(num_clients):
+
+for i in range(num_models):
     system(f"python {program} --base-index {i}")
--- wfedavg_save_clients.py
+++ wfedavg_save_clients.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 from module.net import Net
 from module.learner import Learner
 from module.load_and_split_mnist_dataset import concat_data
-
+from info import model_indices, model_comments, num_models, base_learn_times
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--base-index", type=int)
@@ -16,23 +16,21 @@ use_cuda = True
 
 if __name__ == "__main__":
     print(f"Base index: {base_idx}")
-    num_model_4_indices = [list(range(10))[i:i+4] for i in range(0, 8, 2)]
-    num_model_4_comments = [str(i)+"-"+str(i+4) for i in range(0, 8, 2)]
     # load base model parameter
     base_model = Net()
     base_model.load_state_dict(torch.load(
-        f"model/subenv_{num_model_4_comments[base_idx]}/mnist_cnn.pt"))
-    sub_model_parameters = [base_model.state_dict() for _ in range(4)]
+        f"model/subenv_{model_comments[base_idx]}/mnist_cnn.pt"))
+    sub_model_parameters = [base_model.state_dict() for _ in range(num_models)]
 
     # client training
-    for i, data_idx in enumerate(num_model_4_indices):
+    for i, data_idx in enumerate(model_indices):
         print(f"data index: {data_idx}")
         td, vd = concat_data(data_idx, mode="dataset")
         tdl = DataLoader(td, batch_size=64, shuffle=True)
         vdl = DataLoader(vd, batch_size=64, shuffle=True)
         learner = Learner(tdl, vdl, lr=0.001, log_interval=100, use_cuda=use_cuda)
         learner.model.load_state_dict(sub_model_parameters[i])
-        learner.learn(1)
+        learner.learn(base_learn_times)
         sub_model_parameters[i] = learner.model.state_dict()
-        learner.save(f"./wfed_model_base{base_idx}/subenv_{num_model_4_comments[i]}")
+        learner.save(f"./wfed_model_base{base_idx}/subenv_{model_comments[i]}")
         del learner
